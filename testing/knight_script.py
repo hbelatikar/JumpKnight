7268d8a3e284adfcf84ea44e03dd18a15a81a97a
hbelatikar <hrishikesh.belatikar@gmail.com>
2021-12-08 21:56:03 -0600

    Tested and verified single move

--- testing/knight_script.py
+++ testing/knight_script.py
@@ -5,7 +5,8 @@ os.system("vlog -work work  ../testbench/test_package.sv")
 os.system("vlog -work work  ../testbench/*.sv")
 os.system("vlog -work work  ../testbench/knight_tests/*.sv")
 # os.system("vsim -c -do \"run -all\"  KnightsTour_tb_PWM_NEMO_check");
-os.system("vsim -c -do \"run -all\"  KnightsTour_tb_cal_check");
+# os.system("vsim -c -do \"run -all\"  KnightsTour_tb_cal_check");
+os.system("vsim -c -do \"run -all\"  KnightsTour_tb_moveW1");
 
 
 
@@ -18,6 +19,6 @@ os.system("vsim -c -do \"run -all\"  KnightsTour_tb_cal_check");
 
 
 
-# os.system("vsim -L altera_mf_ver -L lpm_ver KnightsTour_tb_cal_check");
+# os.system("vsim -L altera_mf_ver -L lpm_ver KnightsTour_tb_moveW1");
 #  -do \"waves.do\"
 
