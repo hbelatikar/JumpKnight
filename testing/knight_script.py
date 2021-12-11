fe372898ac584fb9d7d3188e248c33457a4bbb3a
hbelatikar <hrishikesh.belatikar@gmail.com>
2021-12-10 18:35:17 -0600

    Fixed escape character bug in script

--- testing/knight_script.py
+++ testing/knight_script.py
@@ -8,12 +8,12 @@ if platform == "linux" or platform == "linux2":
     os.system("vlog -cover bcst -work work  ../testbench/*.sv")
     os.system("vlog -cover bcst -work work  ../testbench/knight_tests/*.sv")
 
-    os.system("vsim -c -coverage -do \"run -all; coverage save -assert -directive -cvg -codeAll \coverage\ucdb\KnightsTour_tb_PWM_NEMO_check.ucdb; exit\" KnightsTour_tb_PWM_NEMO_check")
-    os.system("vsim -c -coverage -do \"run -all; coverage save -assert -directive -cvg -codeAll \coverage\ucdb\KnightsTour_tb_cal_check.ucdb; exit\"      KnightsTour_tb_cal_check")
-    os.system("vsim -c -coverage -do \"run -all; coverage save -assert -directive -cvg -codeAll \coverage\ucdb\KnightsTour_tb_moveW1.ucdb; exit\"         KnightsTour_tb_moveW1")
-    os.system("vsim -c -coverage -do \"run -all; coverage save -assert -directive -cvg -codeAll \coverage\ucdb\KnightsTour_tb_moveE2FF.ucdb; exit\"       KnightsTour_tb_moveE2FF")
-    os.system("vsim -c -coverage -do \"run -all; coverage save -assert -directive -cvg -codeAll \coverage\ucdb\KnightsTour_tb_tour_chk.ucdb; exit\"       KnightsTour_tb_tour_chk")
-    os.system("vsim -c -coverage -do \"run -all; coverage save -assert -directive -cvg -codeAll \coverage\ucdb\KnightsTour_tb_tour_move.ucdb; exit\"      KnightsTour_tb_tour_move")
+    os.system("vsim -c -coverage -do \"run -all; coverage save -assert -directive -cvg -codeAll \\coverage\\ucdb\\KnightsTour_tb_PWM_NEMO_check.ucdb; exit\" KnightsTour_tb_PWM_NEMO_check")
+    os.system("vsim -c -coverage -do \"run -all; coverage save -assert -directive -cvg -codeAll \\coverage\\ucdb\\KnightsTour_tb_cal_check.ucdb; exit\"      KnightsTour_tb_cal_check")
+    os.system("vsim -c -coverage -do \"run -all; coverage save -assert -directive -cvg -codeAll \\coverage\\ucdb\\KnightsTour_tb_moveW1.ucdb; exit\"         KnightsTour_tb_moveW1")
+    os.system("vsim -c -coverage -do \"run -all; coverage save -assert -directive -cvg -codeAll \\coverage\\ucdb\\KnightsTour_tb_moveE2FF.ucdb; exit\"       KnightsTour_tb_moveE2FF")
+    os.system("vsim -c -coverage -do \"run -all; coverage save -assert -directive -cvg -codeAll \\coverage\\ucdb\\KnightsTour_tb_tour_chk.ucdb; exit\"       KnightsTour_tb_tour_chk")
+    os.system("vsim -c -coverage -do \"run -all; coverage save -assert -directive -cvg -codeAll \\coverage\\ucdb\\KnightsTour_tb_tour_move.ucdb; exit\"      KnightsTour_tb_tour_move")
 
 elif platform == "win32":
     # Windows...
