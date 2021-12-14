495eb0f8768065e6487393ec3228a14fffeaa6eb
Hrishikesh Belatikar <belatikar@tux-150.cae.wisc.edu>
2021-12-13 22:20:06 -0600

    Updated scripts and gitignore

--- postsynth_verif/knight_script_postsynth.py
+++ postsynth_verif/knight_script_postsynth.py
@@ -11,13 +11,20 @@ if platform == "linux" or platform == "linux2":
     os.system("mkdir ucdb")   
 
     # Compile the design and testbench files
-    os.system("vlog -cover bcst -work work  ../synth/*.vg")
+    os.system("vlog -cover bcst -work work  ../synth/outputs/*.vg")
     os.system("vlog -cover bcst -work work  ../src/RemoteComm.sv ../src/KnightPhysics.sv ../src/UAR*.sv ../src/SPI_iNEMO4.sv")
     os.system("vlog -cover bcst -work work  ../testbench/test_package.sv")
     os.system("vlog -cover bcst -work work  ../testbench/knight_tests_postsynth/*.sv")
 
-    os.system("vsim -c KnightsTour_tb_PWM_NEMO_check_postsynth")
+    os.system("vsim KnightsTour_tb_PWM_NEMO_check_postsynth")
     
+    #os.system("vlog -cover bcst -work work  *.vg")
+    #os.system("vlog -cover bcst -work work  RemoteComm.sv KnightPhysics.sv UAR*.sv SPI_iNEMO4.sv")
+    #os.system("vlog -cover bcst -work work  test_package.sv")
+    #os.system("vlog -cover bcst -work work  *_postsynth.sv")
+
+    #os.system("vsim -c KnightsTour_tb_PWM_NEMO_check_postsynth")
+	
 else:
     # Could not find an appropriate OS to run the script on. Exit script.
     print("Couldn't find an operating system matching the specification.")
